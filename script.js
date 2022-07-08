5e22bd3f357fe50237dc1c5ba520305f5a3af8e6
Naime Medjek <naimemedjek@gmail.com>
2022-07-08 23:42:40 +0200

    render static content

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const navbar = (
-    <nav>
-        <h1>My Brand</h1>
-            <ul>
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-    </nav>
+
+const content = (
+    <div>
+        <img src="react-logo.png" width = "40px" />
+        <h1>Fun Facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
 )
 
 
-ReactDOM.render(
-    navbar,
-document.getElementById("root"))
+ReactDOM.render(content,document.getElementById("root"))
 
 
 
